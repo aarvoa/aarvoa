1be21594fc975ecbd8307b7cc3d933cbcc0ffcbd
aarvoa <aaravsreekanth@outlook.com>
2025-11-23 21:32:04 +0100

    steve ai

--- genai/steve_ai.py
+++ genai/steve_ai.py
@@ -1,43 +1,57 @@
-import speech_recognition as sr
-import pyttsx3
 import json
-from speech_recognition.recognizers import google
-listener = sr.Recognizer()
-engine = pyttsx3.init()
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
 
+import pyttsx3
+import speech_recognition as sr
+from streamui import ask_gemini
+
+
+
+
+#reusable code
 def talk(text):
+    #this is a function/ it takes one argument called text
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[1].id)
     engine.say(text)
     engine.runAndWait()
+    del(engine)
 
 def take_command():
+    listener = sr.Recognizer()
     try:
-        with sr.Microphone() as source:
+        with sr.Microphone() as source: #with is my manager who will take care of my resource which is my mic
             print("listening...")
-            voice = listener.listen(source)
+            voice = listener.listen(source=source,phrase_time_limit=10)
             # print(type(voice))
             full_command = listener.recognize_vosk(audio_data=voice,language="en")
             full_command = full_command.lower()
             command = json.loads(full_command)['text']
-
             if 'steve' in command:
-                command =  command.replace('alexa', '')
-                print(command)
-            else:
-                print("Steve not detected")
-
+                command =  command.replace('steve', '')
+                return command
+            if 'exit' in command:
+                print("Tschuss...")
+                talk("Good Bye")
+                exit(0)
+            return "Steve Not Found"
     except Exception as e:
         print(e)
-    return command
+        exit(0)
+
+
 
 def run_steve():
-    command = take_command()
-    print(command)
-    if 'play' in command:
-        song = command.replace('play', '')
-        talk('playing ' + song)
-        print('playing')
+    while True:
+        command = take_command()
+        if command=="Steve Not Found":
+            print("Call me Steve...")
+        else:
+            print("You :" + command)
+            ans="Today is a beautiful day"
+            # ans = ask_gemini(q=command)
 
 
+            print("Steve :"+ans)
+            talk(ans)
 run_steve()
--- genai/streamui.py
+++ genai/streamui.py
@@ -1,14 +1,25 @@
-import streamlit as st
+# import streamlit as st
 from langchain_google_genai import ChatGoogleGenerativeAI
 from decouple import config
+
+from genai.streamuiPrompt import prompt
 
 GOOGLE_GEMINI_KEY = config("GOOGLE_GEMINI_KEY")
 llm=ChatGoogleGenerativeAI(model="gemini-2.5-flash", google_api_key=GOOGLE_GEMINI_KEY)
 
-st.title("ARMaxAI")
+# st.title("ARMaxAI")
 
-question = st.text_input("Your Question")
+# question = st.text_input("Your Question")
 
-if question:
-    response = llm.invoke(question)
-    st.write(response.content)
+# if question:
+#     response = llm.invoke(question)
+#     st.write(response.content)
+
+def ask_gemini(q):
+    p = "Answer the question in less than 50 words.The question is -"
+    full_question = p + q
+    if q:
+        answer = llm.invoke(full_question)
+        return answer.content
+    else:
+        return "Sorry. I did not get you."
