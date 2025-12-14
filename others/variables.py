3a1e2f799534c8faef9e19ec78368ad674c31e80
aarvoa <aaravsreekanth@outlook.com>
2025-12-14 19:35:12 +0100

    tick

--- others/variables.py
+++ others/variables.py
@@ -1,14 +1,1 @@
-
-#global variable
-name="sreekanth"
-def test():
-    # name="sreekanth"
-    print("test ",name)
-
-def test2():
-    name="fluffy" #local variable
-    print("test ",name)
-
-test()
-test2()
-test()
+print('helolo world i am aarav how are you?')
